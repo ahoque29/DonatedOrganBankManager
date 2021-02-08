8b66605863dd97f067deb2edfb38866be54eb091
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-08 10:56:28 +0000

    initialised the patient class

---
+++ HospitalData/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalData
+{
+	public class Patient
+	{
+		public Patient()
+		{
+
+		}
+
+		public string Title { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string PostCode { get; set; }
+		public string BloodType { get; set; }
+	}
+}
