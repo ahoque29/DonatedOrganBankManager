d32befbafbdfa7b511055923b0415843ce767dc9
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-08 11:01:41 +0000

    Initialised the organ table

---
+++ HospitalData/Organ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalData
+{
+	public partial class Organ
+	{
+		public Organ()
+		{
+
+		}
+
+		public string Name { get; set; }
+		public string Type { get; set; }
+
+		public bool IsAgeChecked { get; set; } = true;
+
+	}
+}
