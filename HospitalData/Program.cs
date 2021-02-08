a1425fb396c06651556dcae9b2f8337b795f4e83
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-08 14:17:04 +0000

    populated the organ table via the main method

--- HospitalData/Program.cs
+++ HospitalData/Program.cs
@@ -9,24 +9,34 @@ namespace HospitalData
 		{
 			using (var db = new HospitalContext())
 			{
-				db.Add(new Patient()
-				{
-					Title = "Dr",
-					LastName = "Burris",
-					FirstName = "Joanne",
-					DateOfBirth = new DateTime(1968, 07, 22),
-					Address = "5 Church Street",
-					City = "Ashland",
-					PostCode = "MK6 4AR",
-					Phone = "01908 364751",
-					BloodType = "AB",
-				});
+				//db.Add(new Patient()
+				//{
+				//	Title = "Dr",
+				//	LastName = "Burris",
+				//	FirstName = "Joanne",
+				//	DateOfBirth = new DateTime(1968, 07, 22),
+				//	Address = "5 Church Street",
+				//	City = "Ashland",
+				//	PostCode = "MK6 4AR",
+				//	Phone = "01908 364751",
+				//	BloodType = "AB",
+				//});
 
 				//var priya = db.Patients.Where(f => f.FirstName == "Priya").FirstOrDefault<Patient>();
 				//priya.Title = "Ms";
 
 				//var keaton = db.Patients.Find(1);
 				//keaton.Phone = "01582 635028";
+
+				db.Add(new Organ()
+				{
+					Name = "Platelets",
+					Type = "Tissue",
+					IsAgeChecked = false
+				});
+
+				//var cornea = db.Organs.Where(n => n.Name == "Cornea").FirstOrDefault<Organ>();
+				//cornea.IsAgeChecked = false;
 
 				db.SaveChanges();
 			}
