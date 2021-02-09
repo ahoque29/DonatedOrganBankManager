39af504c3c13b3681fbcf677b2b8764243f92c47
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-09 23:50:49 +0000

    initialised the OrganManager.cs class and made the RetrieveAllOrgans() method.

---
+++ HospitalManagement/OrganManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HospitalData;
+
+
+namespace HospitalManagement
+{
+	public class OrganManager
+	{
+		public Organ SelectedOrgan { get; set; }
+
+		public List<Organ> RetrieveAllOrgans()
+		{
+			using (var db = new HospitalContext())
+			{
+				return db.Organs.ToList();
+			}
+		}
+	}
+}
