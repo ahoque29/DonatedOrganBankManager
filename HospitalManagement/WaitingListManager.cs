b564d9ac02dc3cbbe2922d9147a8ecf0f9fe196c
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-10 01:05:17 +0000

    added the AgeRangeFinder() in WaitingListManager which returns an age range as a string when passed an age.

--- HospitalManagement/WaitingListManager.cs
+++ HospitalManagement/WaitingListManager.cs
@@ -14,5 +14,37 @@ namespace HospitalManagement
 				return db.Waitings.ToList();
 			}
 		}
+
+		public string AgeRangeFinder(int age)
+		{
+			if (age < 0)
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			else if (age <= 1)
+			{
+				return "Newborn or Infant";
+			}
+			else if (age <= 3)
+			{
+				return "Toddler";
+			}
+			else if (age <= 5)
+			{
+				return "Preschooler";
+			}
+			else if (age <= 12)
+			{
+				return "Child";
+			}
+			else if (age <= 19)
+			{
+				return "Teenager";
+			}
+			else
+			{
+				return "Adult";
+			}
+		}
 	}
 }
