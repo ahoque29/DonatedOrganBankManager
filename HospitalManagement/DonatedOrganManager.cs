93ea6f1c9cf1cd2f92036c42e65aa3ac55f7bc78
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-20 22:37:35 +0000

    removed unused methods from DonatedOrganManager.

--- HospitalManagement/DonatedOrganManager.cs
+++ HospitalManagement/DonatedOrganManager.cs
@@ -9,28 +9,6 @@ namespace HospitalManagement
 	{
 		public DonatedOrgan SelectedDonatedOrgan { get; set; }
 
-		public void CreateDonatedOrgan(int organId,
-			string bloodType,
-			int donorAge,
-			DateTime donationDate)
-		{
-			var newDonatedOrgan = new DonatedOrgan()
-			{
-				OrganId = organId,
-				BloodType = bloodType,
-				DonorAge = donorAge,
-				DonationDate = donationDate,
-			};
-
-			using (var db = new HospitalContext())
-			{
-				db.DonatedOrgans.Add(newDonatedOrgan);
-				db.SaveChanges();
-			}
-		}
-
-		// unused method!!
-		// Method overload - allows DonatedOrgan creation with the name of the organ instead of organId
 		public void CreateDonatedOrgan(string organName,
 			string bloodType,
 			int donorAge,
@@ -40,33 +18,19 @@ namespace HospitalManagement
 			{
 				var organ = db.Organs.Where(o => o.Name == organName).FirstOrDefault();
 
-				CreateDonatedOrgan(organ.OrganId,
-					bloodType,
-					donorAge,
-					donationDate);
+				var newDonatedOrgan = new DonatedOrgan()
+				{
+					OrganId = organ.OrganId,
+					BloodType = bloodType,
+					DonorAge = donorAge,
+					DonationDate = donationDate,
+				};
+
+				db.DonatedOrgans.Add(newDonatedOrgan);
+				db.SaveChanges();
 			}
 		}
 
-		// unused
-		public void UpdateDonatedOrgan(int donatedOrganId,
-			string organName,
-			string bloodType,
-			int donorAge,
-			DateTime donationDate)
-		{
-			using (var db = new HospitalContext())
-			{
-				SelectedDonatedOrgan = db.DonatedOrgans.Where(d => d.DonatedOrganId == donatedOrganId).FirstOrDefault();
-				var organ = db.Organs.Where(o => o.Name == organName).FirstOrDefault();
-
-				SelectedDonatedOrgan.OrganId = organ.OrganId;
-				SelectedDonatedOrgan.BloodType = bloodType;
-				SelectedDonatedOrgan.DonorAge = donorAge;
-				SelectedDonatedOrgan.DonationDate = donationDate;
-			}
-		}
-
-		//unused
 		public void DeleteDonatedOrgan(int donatedOrganId)
 		{
 			using (var db = new HospitalContext())
@@ -85,25 +49,6 @@ namespace HospitalManagement
 			using (var db = new HospitalContext())
 			{
 				return db.DonatedOrgans.ToList();
-			}
-		}
-
-		// unused
-		public int DonatedOrgansCount(string organName)
-		{
-			using (var db = new HospitalContext())
-			{
-				bool organExists = db.Organs.Any(o => o.Name == organName);
-				if (organExists)
-				{
-					var organ = db.Organs.Where(o => o.Name == organName).FirstOrDefault<Organ>();
-
-					return db.DonatedOrgans.Where(d => d.OrganId == organ.OrganId).Count();
-				}
-				else
-				{
-					throw new ArgumentException();
-				}
 			}
 		}
 
