6ceb7f3707c1278f741f910b5ee544121f399416
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-03 16:10:22 +0100

    added an interface for the service layer for donated organs

---
+++ HospitalData/Services/Interfaces/IDonatedOrganService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalData.Services.Interfaces
+{
+	public interface IDonatedOrganService
+	{
+		void AddDonatedOrgan(DonatedOrgan donatedOrgan);
+		void RemoveDonatedOrgan(int donatedOrganId);
+		List<DonatedOrgan> GetDonatedOrgansList();
+	}
+}
