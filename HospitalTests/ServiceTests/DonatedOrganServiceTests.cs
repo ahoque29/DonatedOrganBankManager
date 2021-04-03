2eb9c550059b82ff85a8cbeec90c3ca64a8cec86
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-03 18:58:05 +0100

    initialised donatedorganservice test

--- HospitalTests/ServiceTests/DonatedOrganServiceTests.cs
+++ HospitalTests/ServiceTests/DonatedOrganServiceTests.cs
@@ -11,5 +11,19 @@ namespace HospitalTests.ServiceTests
 	[TestFixture]
 	public class DonatedOrganServiceTests
 	{
+		private HospitalContext _context;
+		private DonatedOrganService _donatedOrganService;
+
+		[OneTimeSetUp]
+		public void OneTimeSetup()
+		{
+			var options = new DbContextOptionsBuilder<HospitalContext>()
+				.UseInMemoryDatabase(databaseName: "Hospital_Fake")
+				.Options;
+
+			_context = new HospitalContext(options);
+
+		}
+
 	}
 }
