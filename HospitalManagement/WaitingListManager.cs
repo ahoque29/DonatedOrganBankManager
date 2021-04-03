68ff8f9f6ba3c85c80fb9f6e47842cf154677210
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-03 13:58:24 +0100

    Added xml comments to WaitingListManager.cs

--- HospitalManagement/WaitingListManager.cs
+++ HospitalManagement/WaitingListManager.cs
@@ -23,7 +23,18 @@ namespace HospitalManagement
 
 		#region Create, Delete, Retrieve, Set
 
-		// Create
+		/// <summary>
+		/// Creates a new waiting list entry.
+		/// </summary>
+		/// <param name="patientId">
+		/// Patient Id
+		/// </param>
+		/// <param name="organId">
+		/// Organ Id
+		/// </param>
+		/// <param name="dateOfEntry">
+		/// Date of when the enty was added.
+		/// </param>
 		public void CreateWaiting(int patientId,
 			int organId,
 			DateTime dateOfEntry)
@@ -38,18 +49,35 @@ namespace HospitalManagement
 			_service.AddWaiting(newWaiting);
 		}
 
-		// Retrieve
+		/// <summary>
+		/// Retrieves the waiting list stored in the database.
+		/// </summary>
+		/// <returns>
+		/// Waiting list.
+		/// </returns>
 		public List<Waiting> RetrieveAllWaitings()
 		{
 			return _service.GetWaitingList();
 		}
 
-		// Delete
+		/// <summary>
+		/// Deletes a waiting list entry.
+		/// </summary>
+		/// <param name="waitingId">
+		/// Id of the waiting to be removed.
+		/// </param>
 		public void DeleteWaiting(int waitingId)
 		{
 			_service.RemoveWaiting(waitingId);
 		}
 
+		/// <summary>
+		/// Sets a given object as a waiting.
+		/// Used for front-end
+		/// </summary>
+		/// <param name="selectedItem">
+		/// Object to be set as waiting.
+		/// </param>
 		public void SetSelectedWaiting(object selectedItem)
 		{
 			SelectedWaiting = (Waiting)selectedItem;
