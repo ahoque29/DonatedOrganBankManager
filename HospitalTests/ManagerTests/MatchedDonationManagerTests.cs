034cabe27cce0db10bd4045c0ebfd84d4dd1d313
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-05 14:15:56 +0100

    Added tests for MatchedDonationManager

--- HospitalTests/ManagerTests/MatchedDonationManagerTests.cs
+++ HospitalTests/ManagerTests/MatchedDonationManagerTests.cs
@@ -1,14 +1,28 @@
 ﻿using HospitalData;
+using HospitalData.Services;
 using HospitalManagement;
+using Moq;
 using NUnit.Framework;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace HospitalTests
 {
 	[TestFixture]
 	public class MatchedDonationManagerTests
 	{
+		[Test]
+		public void RetrieveAllMatchedDonations_ReturnsAListOfMatchedDonations()
+		{
+			var mockMatchedDonationsService = new Mock<IMatchedDonationService>();
+			mockMatchedDonationsService.Setup(m => m.GetMatchedDonationsList())
+				.Returns(new List<MatchedDonation>());
+			
+			var _matchedDonationManager = new MatchedDonationManager(mockMatchedDonationsService.Object);
+			var result = _matchedDonationManager.RetrieveAllMatchedDonations();
 
+			Assert.That(result, Is.TypeOf<List<MatchedDonation>>());
+
+		}
 	}
 }
