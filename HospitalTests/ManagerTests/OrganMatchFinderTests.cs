b8171d0f0f3fead5357a0ea6dff78ca0cb9713e3
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-09 01:22:32 +0100

    moved the age range tests to OrganMatchFinderTests and deleted the second test folder

--- HospitalTests/ManagerTests/OrganMatchFinderTests.cs
+++ HospitalTests/ManagerTests/OrganMatchFinderTests.cs
@@ -3,8 +3,8 @@ using HospitalData.Services;
 using HospitalManagement;
 using Moq;
 using NUnit.Framework;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 
 namespace HospitalTests.ManagerTests
 {
@@ -31,13 +31,12 @@ namespace HospitalTests.ManagerTests
 			organMatchFinder.HasOrganList(It.IsAny<int>());
 
 			mockOrganMatchFinderService.Verify(o => o.GetHasOrganList(It.IsAny<int>()), Times.Once);
-
 		}
 
 		[Test]
 		public void HasOrgan_CallsOrganMatchFinderSeriveGetHasOrganlist_WithCorrectParameters()
 		{
-			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
 			mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(3))
 				.Returns(new List<DonatedOrgan>());
 
@@ -51,7 +50,7 @@ namespace HospitalTests.ManagerTests
 		[Test]
 		public void HasOrgan_CallsOrganMatchFinderServiceGetHasOrganList_Once()
 		{
-			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
 			mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(It.IsAny<int>()))
 				.Returns(new List<DonatedOrgan>());
 
@@ -60,6 +59,71 @@ namespace HospitalTests.ManagerTests
 			organMatchFinder.HasOrgan(It.IsAny<int>());
 
 			mockOrganMatchFinderService.Verify(o => o.GetHasOrganList(It.IsAny<int>()), Times.Once);
+		}
+
+		[Test]
+		public void HasOrgan_ReturnsTrue_WhenGetHasOrganListReturnsAListOfDonatedOrgans()
+		{
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
+			mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(It.IsAny<int>()))
+				.Returns(new List<DonatedOrgan>
+				{
+					It.IsAny<DonatedOrgan>()
+				});
+
+			var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
+
+			var result = organMatchFinder.HasOrgan(It.IsAny<int>());
+
+			Assert.That(result, Is.True);
+		}
+
+		[Test]
+		public void HasOrgan_ReturnsFalse_WhenGetHasOrganListReturnsAnEmptyList()
+		{
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
+			mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(It.IsAny<int>()))
+				.Returns(new List<DonatedOrgan>());
+
+			var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
+
+			var result = organMatchFinder.HasOrgan(It.IsAny<int>());
+
+			Assert.That(result, Is.False);
+		}
+
+		[TestCase(0, "Newborn or Infant")]
+		[TestCase(1, "Newborn or Infant")]
+		[TestCase(3, "Toddler")]
+		[TestCase(5, "Preschooler")]
+		[TestCase(12, "Child")]
+		[TestCase(19, "Teenager")]
+		[TestCase(20, "Adult")]
+		[TestCase(99, "Adult")]
+		public void AgeRangeFinder_ReturnsCorrectAgeRage_WhenAgeIsPassed(int a, string expectedResult)
+		{
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
+			var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
+
+			var result = organMatchFinder.AgeRangeFinder(a);
+			Assert.That(expectedResult, Is.EqualTo(result));
+		}
+
+		[TestCase("2020-02-10", "Newborn or Infant")]
+		[TestCase("2020-06-23", "Newborn or Infant")]
+		[TestCase("2018-09-01", "Toddler")]
+		[TestCase("2017-10-07", "Preschooler")]
+		[TestCase("2010-06-10", "Child")]
+		[TestCase("2003-03-27", "Teenager")]
+		[TestCase("1986-02-03", "Adult")]
+		[TestCase("1919-09-03", "Adult")]
+		public void AgeRangeFinder_ReturnsCorrectAgeRage_WhenDobIsPassed(DateTime dob, string expectedResult)
+		{
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
+			var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
+
+			var result = organMatchFinder.AgeRangeFinder(dob);
+			Assert.AreEqual(result, expectedResult);
 		}
 	}
 }
