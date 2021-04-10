aca258a7161ef02b975b6dacc9b58adbf8b1d4b2
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-10 21:22:27 +0100

    moved some tests from matched donation manager to organmatchfinder tests

--- HospitalTests/ManagerTests/OrganMatchFinderTests.cs
+++ HospitalTests/ManagerTests/OrganMatchFinderTests.cs
@@ -11,86 +11,38 @@ namespace HospitalTests.ManagerTests
 	[TestFixture]
 	public class OrganMatchFinderTests
 	{
-		//[Test]
-		//public void HasOrganList_CallsOrganMatchFinderServiceGetHasOrganList_WithCorrectParameters()
-		//{
-		//	var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
-		//	var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
+		[Test]
+		public void CreateMatchedDonation_CallsIOrganMatchFinderServiceAddMatchedDonation_WithCorrectParameters()
+		{
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
+			var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
 
-		//	organMatchFinder.HasOrganList(3);
+			organMatchFinder.CreateMatchedDonation(2,
+				3,
+				new DateTime(1900, 01, 01));
 
-		//	mockOrganMatchFinderService.Verify(o => o.GetHasOrganList(3));
-		//}
+			var matchedDonationToBeAdded = new MatchedDonation()
+			{
+				PatientId = 2,
+				DonatedOrganId = 3,
+				DateOfMatch = new DateTime(1900, 01, 01)
+			};
 
-		//[Test]
-		//public void HasOrganList_CallsOrganMatchFinderServiceGetHasOrganList_Once()
-		//{
-		//	var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
-		//	var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
+			mockOrganMatchFinderService.Verify(m => m.AddMatchedDonation(matchedDonationToBeAdded));
+		}
 
-		//	organMatchFinder.HasOrganList(It.IsAny<int>());
+		[Test]
+		public void CreateMatchedDonation_CallsIOrganMatchFinderServiceAddMatchedDonation_Once()
+		{
+			var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Loose);
+			var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
 
-		//	mockOrganMatchFinderService.Verify(o => o.GetHasOrganList(It.IsAny<int>()), Times.Once);
-		//}
+			organMatchFinder.CreateMatchedDonation(It.IsAny<int>(),
+				It.IsAny<int>(),
+				It.IsAny<DateTime>());
 
-		//[Test]
-		//public void HasOrgan_CallsOrganMatchFinderSeriveGetHasOrganlist_WithCorrectParameters()
-		//{
-		//	var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
-		//	mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(3))
-		//		.Returns(new List<DonatedOrgan>());
-
-		//	var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
-
-		//	organMatchFinder.HasOrgan(3);
-
-		//	mockOrganMatchFinderService.Verify(o => o.GetHasOrganList(3));
-		//}
-
-		//[Test]
-		//public void HasOrgan_CallsOrganMatchFinderServiceGetHasOrganList_Once()
-		//{
-		//	var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
-		//	mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(It.IsAny<int>()))
-		//		.Returns(new List<DonatedOrgan>());
-
-		//	var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
-
-		//	organMatchFinder.HasOrgan(It.IsAny<int>());
-
-		//	mockOrganMatchFinderService.Verify(o => o.GetHasOrganList(It.IsAny<int>()), Times.Once);
-		//}
-
-		//[Test]
-		//public void HasOrgan_ReturnsTrue_WhenGetHasOrganListReturnsAListOfDonatedOrgans()
-		//{
-		//	var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
-		//	mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(It.IsAny<int>()))
-		//		.Returns(new List<DonatedOrgan>
-		//		{
-		//			It.IsAny<DonatedOrgan>()
-		//		});
-
-		//	var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
-
-		//	var result = organMatchFinder.HasOrgan(It.IsAny<int>());
-
-		//	Assert.That(result, Is.True);
-		//}
-
-		//[Test]
-		//public void HasOrgan_ReturnsFalse_WhenGetHasOrganListReturnsAnEmptyList()
-		//{
-		//	var mockOrganMatchFinderService = new Mock<IOrganMatchFinderService>(MockBehavior.Strict);
-		//	mockOrganMatchFinderService.Setup(o => o.GetHasOrganList(It.IsAny<int>()))
-		//		.Returns(new List<DonatedOrgan>());
-
-		//	var organMatchFinder = new OrganMatchFinder(mockOrganMatchFinderService.Object);
-
-		//	var result = organMatchFinder.HasOrgan(It.IsAny<int>());
-
-		//	Assert.That(result, Is.False);
-		//}
+			mockOrganMatchFinderService.Verify(m => m.AddMatchedDonation(It.IsAny<MatchedDonation>()), Times.Once);
+		}
 
 		[TestCase(0, "Newborn or Infant")]
 		[TestCase(1, "Newborn or Infant")]
