cdbecd2af1d4c3020eb69b946a696aa8537ae1c4
Ashib Hoque <a.hoque29@hotmail.com>
2021-04-22 12:46:59 +0100

    Streamlined the test cases for ListCompatibleOrgans() and added comments to clarify the tests

--- HospitalTests/ManagerTests/OrganMatchFinderTests.cs
+++ HospitalTests/ManagerTests/OrganMatchFinderTests.cs
@@ -120,40 +120,72 @@ namespace HospitalTests.ManagerTests
 			Assert.That(result, Is.TypeOf<List<DonatedOrgan>>());
 		}
 
-		[TestCase(1, 2, true, "2011-04-16", 90, "A", "B", false)]
-		[TestCase(1, 1, true, "2011-04-16", 90, "A", "B", false)]
-		[TestCase(1, 2, false, "2011-04-16", 90, "A", "B", false)]
-		[TestCase(1, 2, true, "2011-04-16", 10, "A", "B", false)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "A", "B", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "A", "B", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "O", "A", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "O", "B", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "O", "AB", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "A", "AB", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "B", "A", false)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "B", "AB", false)]
-		[TestCase(1, 2, true, "2011-04-16", 90, "AB", "A", false)]
-		[TestCase(1, 1, true, "2011-04-16", 90, "AB", "A", false)]
-		[TestCase(1, 2, false, "2011-04-16", 90, "AB", "A", false)]
-		[TestCase(1, 2, true, "2011-04-16", 10, "AB", "A", false)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "A", true)]
-		[TestCase(1, 1, true, "2011-04-16", 10, "AB", "A", true)]
-		[TestCase(1, 1, true, "2020-02-10", 0, "AB", "A", true)]
-		[TestCase(1, 1, true, "2020-06-23", 1, "AB", "A", true)]
-		[TestCase(1, 1, true, "2018-09-01", 3, "AB", "A", true)]
-		[TestCase(1, 1, true, "2017-10-07", 5, "AB", "A", true)]
-		[TestCase(1, 1, true, "2010-06-10", 12, "AB", "A", true)]
-		[TestCase(1, 1, true, "2003-03-27", 19, "AB", "A", true)]
-		[TestCase(1, 1, true, "1986-02-03", 20, "AB", "A", true)]
-		[TestCase(1, 1, true, "1919-09-03", 99, "AB", "A", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "O", "O", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "A", "O", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "B", "O", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "O", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "A", "A", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "B", "B", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "B", true)]
-		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "AB", true)]
+		/// <summary>
+		/// Tests the filters and the appropriate response.<br/>
+		/// Organ filter passes when waitingOrganId == organId.<br/>
+		/// Age filter passes when isAgeChecked == false OR isAgeChecked == true and the age of the patient (calculated from patientDob) is comparable to donorAge.<br/>
+		/// Blood type filter passes when patientBloodType and DonorBloodType are compatible.<br/>
+		/// <br/>
+		/// expectedResult is true when all filters pass.
+		/// </summary>
+		/// <param name="waitingOrganId">
+		/// Organ id of waiting list entry.
+		/// </param>
+		/// <param name="organId">
+		/// Organ Id of donated organ candidate.
+		/// </param>
+		/// <param name="isAgeChecked">
+		/// Bool that checks if Age filter is used or not.
+		/// </param>
+		/// <param name="patientDob">
+		/// Date of birth of patient.
+		/// </param>
+		/// <param name="donorAge">
+		/// Age of donor at time of donation.
+		/// </param>
+		/// <param name="patientBloodType">
+		/// Blood type of patient.
+		/// </param>
+		/// <param name="donorBloodType">
+		/// Blood type of donor.
+		/// </param>
+		/// <param name="expectedResult">
+		/// Expected result.
+		/// </param>
+		[TestCase(1, 2, true, "2011-04-16", 90, "A", "B", false)] // No filters pass
+		[TestCase(1, 1, true, "2011-04-16", 90, "A", "B", false)] // Organ filter passes, rest fails
+		[TestCase(1, 2, false, "2011-04-16", 90, "A", "B", false)] // Age filter passes, rest fails
+		[TestCase(1, 2, true, "2011-04-16", 10, "A", "B", false)] // Age filter passes, rest fails
+		[TestCase(1, 1, false, "2011-04-16", 90, "A", "B", false)] // Organ and age filter pass, blood type filter fails
+		[TestCase(1, 1, true, "2011-04-16", 10, "A", "B", false)] // Organ and age filter pass, blood type filter fails
+		[TestCase(1, 1, true, "2011-04-16", 10, "O", "A", false)] // Organ and age filter pass, blood type filter fails (testing different blood types)
+		[TestCase(1, 1, true, "2011-04-16", 10, "B", "A", false)] // Organ and age filter pass, blood type filter fails (testing different blood types)
+		[TestCase(1, 1, true, "2011-04-16", 10, "O", "B", false)] // Organ and age filter pass, blood type filter fails (testing different blood types)
+		[TestCase(1, 1, true, "2011-04-16", 10, "O", "AB", false)] // Organ and age filter pass, blood type filter fails (testing different blood types)
+		[TestCase(1, 1, true, "2011-04-16", 10, "A", "AB", false)] // Organ and age filter pass, blood type filter fails (testing different blood types)
+		[TestCase(1, 1, true, "2011-04-16", 10, "B", "AB", false)] // Organ and age filter pass, blood type filter fails (testing different blood types)
+		[TestCase(1, 2, true, "2011-04-16", 90, "A", "A", false)] // Blood type filter passes, rest fails
+		[TestCase(1, 1, true, "2011-04-16", 90, "A", "A", false)] // Organ and blood type filter pass, age filter fails
+		[TestCase(1, 2, false, "2011-04-16", 90, "A", "A", false)] // Age and blood type filter pass, organ filter fails
+		[TestCase(1, 2, true, "2011-04-16", 10, "A", "A", false)] // Age and blood type filter pass, organ filter fails
+		[TestCase(1, 1, false, "2011-04-16", 90, "A", "A", true)] // All filters pass
+		[TestCase(1, 1, true, "2011-04-16", 10, "A", "A", true)] // All filters pass
+		[TestCase(1, 1, true, "2020-02-10", 0, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "2020-06-23", 1, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "2018-09-01", 3, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "2017-10-07", 5, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "2010-06-10", 12, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "2003-03-27", 19, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "1986-02-03", 20, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, true, "1919-09-03", 20, "A", "A", true)] // All filters pass (testing different range of ages)
+		[TestCase(1, 1, false, "2011-04-16", 90, "O", "O", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "A", "O", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "B", "O", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "O", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "A", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "B", "B", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "B", true)] // All filters pass (testing different blood types)
+		[TestCase(1, 1, false, "2011-04-16", 90, "AB", "AB", true)] // All filters pass (testing different blood types)
 		public void ListCompatibleOrgans_FilterTests(int waitingOrganId,
 			int organId,
 			bool isAgeChecked,
@@ -168,7 +200,7 @@ namespace HospitalTests.ManagerTests
 			mockOrganMatchFinderService.Setup(o => o.GetWaiting(It.IsAny<int>()))
 				.Returns(new Waiting() { OrganId = waitingOrganId });
 			mockOrganMatchFinderService.Setup(o => o.GetOrgan(It.IsAny<Waiting>()))
-				.Returns(new Organ() { OrganId = organId, IsAgeChecked = isAgeChecked });
+				.Returns(new Organ() { OrganId = waitingOrganId, IsAgeChecked = isAgeChecked });
 			mockOrganMatchFinderService.Setup(o => o.GetPatient(It.IsAny<Waiting>()))
 				.Returns(new Patient() { DateOfBirth = patientDob, BloodType = patientBloodType });
 			mockOrganMatchFinderService.Setup(o => o.GetDonatedOrgans())
